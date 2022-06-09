83afd65eb012c3a776c60dca3177ff6bcc2e7984
Yves Botshimbo <ybidotsh@gmail.com>
2022-06-08 21:23:45 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
   async componentDidMount(){
     const response = await api.get(''); 
 
-    console.log(response.data);
+    //console.log(response.data);
 
     this.setState({ contatos: response.data});
   }
@@ -16,14 +16,27 @@ class App extends Component{
 
     const { contatos } = this.state;
 
+    console.log(contatos);
+
     return(
       <div>
         <h1>Lista dos contatos</h1>
+        <ul style = {{listStyleType: "none", margin:0, padding: 0}}>
+          {contatos.map(contato => (
+            <nav>
+            <li key = {contatos._id}>
+            <p>Imagem destacada: {contatos.imagemdestacada}</p>
+            <p>Titulo: {contatos.titulo}</p>
+            <p>Link para postagem: {contatos.linkparapostagem}</p>
+            </li>
+            </nav>
+
+          ))}
+
+        </ul>
         
-      
 
       </div>
-
     );
   };
 };
